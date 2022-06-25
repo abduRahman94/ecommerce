6f96fd0737eef5b964db4c65dcd9868cd5baf02e
abduRahman94 <sabdouseye@gmail.com>
2022-06-25 13:03:44 +0000

    Refactoring models

--- commerce/shop/models.py
+++ commerce/shop/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 
 class Categorie(models.Model):
+    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=200, null=True)
 
 
 class SousCategorie(models.Model):
+    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=200, null=True)
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, null=True, related_name='sous_categories')
 
 
 class Produit(models.Model):
+    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=200, null=True)
     sous_categorie = models.ForeignKey(SousCategorie, on_delete=models.CASCADE, null=True, related_name='produits')
     prix = models.FloatField(default=0)
@@ -28,4 +31,3 @@ class Commande(models.Model):
     date = models.DateTimeField(default=None)
     produit = models.ForeignKey(Produit, null=True, on_delete=models.PROTECT)
     client = models.ForeignKey(Client, null=True, on_delete=models.PROTECT, related_name='commandes')
-
