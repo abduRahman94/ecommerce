06d814f69c78c24caa417118fa0e9c0bff483354
abduRahman94 <sabdouseye@gmail.com>
2022-07-02 00:10:28 +0000

    Gestion panier

--- commerce/shop/models.py
+++ commerce/shop/models.py
@@ -29,5 +29,47 @@ class Client(models.Model):
 
 class Commande(models.Model):
     date = models.DateTimeField(default=None)
-    produit = models.ForeignKey(Produit, null=True, on_delete=models.PROTECT)
-    client = models.ForeignKey(Client, null=True, on_delete=models.PROTECT, related_name='commandes')
+    reference_commande = models.CharField(max_length=200)
+    client_id = models.ForeignKey(Client, null=True, on_delete=models.PROTECT, related_name='commandes')
+
+
+class DetailCommande(models.Model):
+    commande = models.ForeignKey(Commande, on_delete=models.CASCADE, null=True, related_name='details_commandes')
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE, null=True, related_name='details_commandes')
+    quantite = models.IntegerField(default=0)
+    prix = models.FloatField(default=0)
+
+
+class Panier(models.Model):
+
+    liste_produits = []
+
+    # add a product to the cart
+    def add_product(self, produit, quantite):
+        self.liste_produits.append({'produit': produit, 'quantite': quantite})
+        return self.liste_produits
+
+    # get list of products in the cart
+    def get_list_products(self):
+        return self.liste_produits
+
+    # remove a product from the cart
+    def remove_product(self, produit):
+        for produit_panier in self.liste_produits:
+            if produit_panier['produit'] == produit:
+                self.liste_produits.remove(produit_panier)
+                return self.liste_produits
+
+    # get the total price of the cart
+    def get_total_price(self):
+        total_price = 0
+        for produit_panier in self.liste_produits:
+            total_price += produit_panier['produit'].prix * produit_panier['quantite']
+        return total_price
+
+    # get the total quantity of the cart
+    def get_total_quantity(self):
+        total_quantity = 0
+        for produit_panier in self.liste_produits:
+            total_quantity += produit_panier['quantite']
+        return total_quantity
